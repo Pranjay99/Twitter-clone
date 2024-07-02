8a6de9d5afe04dc8f3ed872a53af832b9cc6a814
Pranjay seksaria <93260737+Pranjay99@users.noreply.github.com>
2024-07-03 00:49:55 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,11 +7,11 @@ const router = express.Router();
 router.route("/register").post(Register);
 router.route("/login").post(Login);
 router.route("/logout").get(logout);
-router.route("/bookmark/:id").put(isAuthenticated,bookmarks);
+router.route("/bookmark/:id").put(bookmarks);
 router.route("/profile/:id").get(getMyProfile);
 router.route("/otheruser/:id").get(getOtherUsers);
-router.route("/follow/:id").post(isAuthenticated,follow);
-router.route("/unfollow/:id").post(isAuthenticated,unfollow);
+router.route("/follow/:id").post(follow);
+router.route("/unfollow/:id").post(unfollow);
 router.route("/updateprofile/:id").put(updateProfile);
 
 
