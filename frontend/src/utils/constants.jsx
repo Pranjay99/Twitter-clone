fc53e24eecfb3a535436048461e85afc87e92480
Pranjay seksaria <93260737+Pranjay99@users.noreply.github.com>
2024-07-02 16:22:02 +0530

    Update constants.jsx with new server address

--- frontend/src/utils/constants.jsx
+++ frontend/src/utils/constants.jsx
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet";
+export const USER_API_END_POINT = "https://twitter-clone-backend-vx80.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT = "https://twitter-clone-backend-vx80.onrender.com/api/v1/tweet";
 
 export const timeSince = (timestamp) => {
     let time = Date.parse(timestamp);
