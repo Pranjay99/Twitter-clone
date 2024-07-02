91d249c560f3bdc36eb4ed5eeefdbec2e5e1c464
Pranjay seksaria <93260737+Pranjay99@users.noreply.github.com>
2024-07-03 01:13:40 +0530

    Update Leftbar.jsx

--- frontend/src/components/Leftbar.jsx
+++ frontend/src/components/Leftbar.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { CiHome } from "react-icons/ci";
 import { IoMdNotificationsOutline } from "react-icons/io";
-import { IoLogIn } from "react-icons/io5";
-
-import { FaSearch } from "react-icons/fa";
+import { IoLogIn, IoLogOut } from "react-icons/io5";
+import { FaSearch, FaRegBookmark } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
-import { FaRegBookmark } from "react-icons/fa";
-import { IoLogOut } from "react-icons/io5";
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { USER_API_END_POINT } from '../utils/constants';
@@ -25,7 +22,6 @@ const Leftbar = () => {
       dispatch(getUser(null));
       dispatch(getOtherUsers(null));
       dispatch(getMyProfile(null));
-
       navigate('/login');
       toast.success(res.data.message);
     } catch (error) {
@@ -37,14 +33,14 @@ const Leftbar = () => {
     <div className='md:w-[20%] w-[15%]'>
       <div>
         <Link to="/">
-          <img className="md:ml-4 ml-2" width="50" src="https://img.freepik.com/free-vector/twitter-new-2023-x-logo-white-background-vector_1017-45422.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1718755200&semt=ais_user" alt="" />
+          <img className="md:ml-4 ml-2" width="50" src="https://img.freepik.com/free-vector/twitter-new-2023-x-logo-white-background-vector_1017-45422.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1718755200&semt=ais_user" alt="Twitter logo" />
         </Link>
         <div className='my-4'>
           <Link to="/" className='flex items-center md:my-2 hover:bg-gray-300 rounded-full hover:cursor-pointer md:py-3 md:px-4 p-1 ml-3 my-4'>
             <CiHome size="24px" />
             <span className='font-bold hidden sm:block ml-2'> Home</span>
           </Link>
-          <Link to="/trending" className='flex items-center  md:my-2 hover:bg-gray-300 rounded-full hover:cursor-pointer md:py-3 md:px-4 p-1 ml-3 my-4'>
+          <Link to="/trending" className='flex items-center md:my-2 hover:bg-gray-300 rounded-full hover:cursor-pointer md:py-3 md:px-4 p-1 ml-3 my-4'>
             <FaSearch size="20px" />
             <span className='font-bold hidden sm:block ml-2'> Explore</span>
           </Link>
@@ -60,14 +56,19 @@ const Leftbar = () => {
             <FaRegBookmark size="22px" />
             <span className='font-bold hidden sm:block ml-2'> Bookmark</span>
           </Link>
-          <Link to={`/login`} className='flex items-center md:my-2 hover:bg-gray-300 rounded-full hover:cursor-pointer md:py-3 md:px-4 p-1 ml-3 my-4'>
-            <IoLogIn  size="22px" />
-            <span className='font-bold hidden sm:block ml-2'> login</span>
-          </Link>
-          <div onClick={logouthandler} className='flex items-center md:my-2 hover:bg-gray-300 rounded-full hover:cursor-pointer md:py-3 md:px-4 p-1 ml-3 my-4'>
-            <IoLogOut size="24px" />
-            <span className='font-bold hidden sm:block ml-2'> Log out</span>
-          </div>
+          
+          {user ? (
+            <div onClick={logouthandler} className='flex items-center md:my-2 hover:bg-gray-300 rounded-full hover:cursor-pointer md:py-3 md:px-4 p-1 ml-3 my-4'>
+              <IoLogOut size="24px" />
+              <span className='font-bold hidden sm:block ml-2'> Log out</span>
+            </div>
+          ) : (
+            <Link to={`/login`} className='flex items-center md:my-2 hover:bg-gray-300 rounded-full hover:cursor-pointer md:py-3 md:px-4 p-1 ml-3 my-4'>
+              <IoLogIn size="22px" />
+              <span className='font-bold hidden sm:block ml-2'> Login</span>
+            </Link>
+          )}
+          
           <button className='bg-blue-400 px-9 py-2 w-full rounded-full font-bold text-white hidden sm:block'>Post</button>
         </div>
       </div>
@@ -94,7 +95,7 @@ const Leftbar = () => {
         </div>
       </div> */}
     </div>
-  )
-}
+  );
+};
 
 export default Leftbar;
